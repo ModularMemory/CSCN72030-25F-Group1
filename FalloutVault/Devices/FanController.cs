cef20d971ed2128550e38c280cf287ead3f29077
Iris H <ihahn2449@conestogac.on.ca>
2025-11-07 08:41:14 -0500

    Added fan controller and related interface

---
+++ FalloutVault/Devices/FanController.cs
@@ -0,0 +1,61 @@
+﻿using FalloutVault.Devices.Interfaces;
+using FalloutVault.Models;
+
+
+namespace FalloutVault.Devices;
+public class FanController : Device, IFanController
+{
+    //fields
+    private bool _IsOn;
+
+    private double _TargetRpm;
+
+    private double _SpeedRpm;
+
+    //properties
+   
+    public override string Name { get; }
+    public override string Zone { get; }
+    public override EventHandler<DeviceMessage>? OnDeviceMessage { get; set; }
+
+    //TODO: Notification on setters
+    public bool IsOn
+    {
+        get => _IsOn;
+        set => _IsOn = value;
+    }
+
+    public double TargetRpm
+    {
+        get => _TargetRpm;
+        set => _TargetRpm = value;
+    }
+
+    public double SpeedRpm 
+    { 
+        get => _SpeedRpm; 
+    }
+
+    //Constructors
+    public FanController(string name, string zone)
+    {
+        Name = name;
+        Zone = zone;
+    }
+
+    //Methods
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TurnOnFor(TimeSpan time)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TurnOffFor(TimeSpan time)
+    {
+        throw new NotImplementedException();
+    }
+}
