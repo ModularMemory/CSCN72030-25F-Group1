06812187ba369ac1d8e54ebb8ce0039f290f1a6f
KazziBears <justinadakin@gmail.com>
2025-11-08 15:55:32 -0500

    Changed and fixed up speaker

--- FalloutVault/Devices/SpeakerController.cs
+++ FalloutVault/Devices/SpeakerController.cs
@@ -1,48 +1,90 @@
-﻿using FalloutVault.Devices.Interfaces;
-using FalloutVault.Models;
+﻿using System.Diagnostics;
+using FalloutVault.Devices.Interfaces;
+using FalloutVault.Devices.Models;
+using FalloutVault.Eventing.Models;
 
 namespace FalloutVault.Devices;
 
 public class SpeakerController : Device, ISpeakerController
 {
     // Fields
+    private readonly DeviceTimer<bool> _deviceTimer = new();
+    private readonly Lock _timerLock = new();
+    private readonly Watt _speakerWattage;
 
     private bool _activated;
 
     // Properties
 
-    public override string Name { get; }
-    public override string Zone { get; }
-    public override EventHandler<DeviceMessage>? OnDeviceMessage { get; }
-
+   public override DeviceId Id { get; }
     public bool Activated
     {
         get => _activated;
-        set => _activated = value;
+        set
+        {
+            if(!SetField(ref _activated, value)) return;
+
+            PublishMessage(_activated
+                ? new DeviceMessage("Speaker turned on", ValueBoxes.True)
+                : new DeviceMessage("Speaker turned off", ValueBoxes.False)
+             );
+
+            PowerDraw = ComputePowerDraw();
+        }
     }
 
     // Constructors
 
-    public SpeakerController(string name, string zone)
+    public SpeakerController(DeviceId id, Watt speakerWattage)
     {
-        Name = name;
-        Zone = zone;
+        Id = id;
+        _speakerWattage = speakerWattage;
     }
 
     // Methods
 
     public override void Update()
     {
-        throw new NotImplementedException();
+        Debug.Assert(PowerDraw == ComputePowerDraw());
+
+        lock (_timerLock)
+        {
+            if (_deviceTimer.IsRunning)
+            {
+                _deviceTimer.Update();
+
+                if (!_deviceTimer.IsRunning)
+                {
+                    Activated = _deviceTimer.State;
+                }
+            }
+        }
     }
 
+    protected override Watt ComputePowerDraw()
+    {
+        if (!Activated)
+        {
+            return Watt.Zero;
+        }
+
+        return _speakerWattage;
+    }
     public void TurnOnFor(TimeSpan time)
     {
-        throw new NotImplementedException();
+        lock (_timerLock)
+        {
+            _deviceTimer.SetTimer(time, false);
+            Activated = true;
+        }
     }
 
     public void TurnOffFor(TimeSpan time)
     {
-        throw new NotImplementedException();
+        lock ( _timerLock)
+        {
+            _deviceTimer.SetTimer(time, true);
+            Activated = false;
+        }
     }
 }
