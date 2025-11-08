680fc13d10f026955b85bbf8e80e715e2e5b7fcf
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-11-08 02:38:25 -0500

    Add basic PowerController and IPowerController interface

---
+++ FalloutVault/Devices/Interfaces/IPowerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FalloutVault.Eventing.Models;
+
+namespace FalloutVault.Devices.Interfaces;
+public interface IPowerController
+{
+    Watt PowerGeneration { get; }
+    Watt StandardGeneration { get; }
+    double Efficiency { get; }
+}
