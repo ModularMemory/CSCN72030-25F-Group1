a0cc8993cc737f7b264ae620aafbe4be8397f43f
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-11-11 15:10:46 -0500

    Unsubscribe from old inner bus invoke on setter

--- FalloutVault.Tests/Mocks/MockEventBus.cs
+++ FalloutVault.Tests/Mocks/MockEventBus.cs
@@ -6,20 +6,28 @@ namespace FalloutVault.Tests.Mocks;
 internal abstract class MockEventBus<TMessage> : IEventBus<TMessage>
 {
     private readonly List<TMessage> _messages = [];
-    private readonly IEventBus<TMessage> _innerBus = null!;
+    private readonly IEventBus<TMessage>? _innerBus;
 
     public event EventHandler<TMessage>? Handler;
 
     public IReadOnlyList<TMessage> Messages => _messages;
 
-    protected IEventBus<TMessage> InnerBus
+    protected IEventBus<TMessage>? InnerBus
     {
         get => _innerBus;
         init
         {
+            if (_innerBus != null) _innerBus.Handler -= InnerBusOnHandler;
+
             _innerBus = value;
-            _innerBus.Handler += (sender, message) => Handler?.Invoke(sender, message);
+
+            if (_innerBus != null) _innerBus.Handler += InnerBusOnHandler;
         }
+    }
+
+    private void InnerBusOnHandler(object? sender, TMessage message)
+    {
+        Handler?.Invoke(sender, message);
     }
 
     public void Publish(object sender, TMessage data)
