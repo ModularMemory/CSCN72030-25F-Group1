b30163ea13c11c4f6a4d18553d1112efd4e34a45
Alias413 <caviesheppard@gmail.com>
2025-11-22 03:50:37 -0500

    added device messages for ventseal and cropsprinkler

---
+++ FalloutVault/Eventing/Models/DeviceMessage.CropSprinklerController.cs
@@ -0,0 +1,20 @@
+﻿namespace FalloutVault.Eventing.Models;
+
+public partial class DeviceMessage
+{
+    public class CropSprinklerOnChanged : DeviceOnChanged
+    {
+        public LightOnChanged(bool data) : base(data) { }
+        public LightOnChanged(bool data, DateTimeOffset timestamp) : base(data, timestamp) { }
+
+        public override string Message => "Crop Sprinkler state changed";
+    }
+
+    public class DimmerLevelChanged : DeviceMessage
+    {
+        public CropSprinklerSectionChanged(double data) : base(data) { }
+        public CropSprinklerSectionChanged(double data, DateTimeOffset timestamp) : base(data, timestamp) { }
+
+        public override string Message => "Crop Sprinkler Target Section changed";
+    }
+}
---
+++ FalloutVault/Eventing/Models/DeviceMessage.VentSealController.cs
@@ -0,0 +1,20 @@
+﻿namespace FalloutVault.Eventing.Models;
+
+public partial class DeviceMessage
+{
+    public class VentStateChanged : DeviceOnChanged
+    {
+        public VentStateChanged(bool data) : base(data) { }
+        public VentStateChanged(bool data, DateTimeOffset timestamp) : base(data, timestamp) { }
+
+        public override string Message => "Vent state changed";
+    }
+
+    public class VentLockedChanged : DeviceMessage
+    {
+        public VentLockedChanged(double data) : base(data) { }
+        public VentLockedChanged(double data, DateTimeOffset timestamp) : base(data, timestamp) { }
+
+        public override string Message => "Vent Lock state changed";
+    }
+}
