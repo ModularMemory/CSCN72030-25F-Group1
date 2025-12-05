219081cf40dd7e3c7ae4ecbc654c346afdc8aa62
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-12-05 12:56:14 -0500

    Fully fix log search box width

---
+++ FalloutVault.AvaloniaApp/Converters/DivideByConverter.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace FalloutVault.AvaloniaApp.Converters;
+
+public class DivideByConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double d && parameter is string s && double.TryParse(s, out var p))
+        {
+            return d / p;
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
